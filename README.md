5012adc6d9c3c70e33cc5e3fac5ceb378e8556ed
katagis <fox.dealos@gmail.com>
2019-04-16 20:54:31 +0300

    Add initial files

---
+++ README.md
@@ -0,0 +1,4 @@
+# vscode msbuild
+
+## Note:
+Remember to run vscode through "x64 Native Tools Command Prompt for VS 2019"
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    if constexpr ("c++17") {
+        std::cout << "Hello World!\n";
+    }
+}
